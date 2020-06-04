fb4eb557d6fded0be9e82b39363ecfb313fbb3d1
Derryk Boyd <derrykboyd@gmail.com>
2020-06-03 23:46:24 -0600

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+// React
 import React, { useState, useEffect } from 'react';
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+  NavLink
+} from "react-router-dom";
 
+// Firebase
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
+// styles
 import './App.css';
+
+// Components
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyBqJvH9x13wUFJhaBjCqkxUPesQ7Fm0YXg",
@@ -28,7 +41,8 @@ function App() {
     signInOptions: [
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
       firebase.auth.EmailAuthProvider.PROVIDER_ID
-    ]
+    ],
+    'credentialHelper': 'none'
   }
 
   useEffect(() => {
@@ -36,7 +50,7 @@ function App() {
     const unsubscribe = firebaseApp.auth().onAuthStateChanged(user => {
       setUser(user)
       console.log(user)
-      document.getElementById("login-form").classList.remove("hide");
+      // document.getElementById("login-form").classList.remove("hide");
     })
     // unsubscribe to the listener when unmounting
     return () => unsubscribe()
@@ -44,29 +58,59 @@ function App() {
 
 
   return (
-    <div className="App">
-      {!user &&
-        <div id="login-form" className="login-form hide">
-          <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebaseApp.auth()} />
-        </div>
-      }
-      {user &&
-        <header className="App-header">
-          <button onClick={() => firebaseApp.auth().signOut()}>Sign Out</button>
-          <p>Welcome {user.displayName}</p>
-          <p>
-            App for tracking Ultimate statistics
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-        </header>}
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/' exact>
+          <div className="App">
+            <header className="App-header">
+              {user &&
+                <div>
+                  <button onClick={() => firebaseApp.auth().signOut()}>Sign Out</button>
+                  <p>Welcome {user.displayName}</p>
+                </div>
+              }
+              <p>
+                App for tracking Ultimate statistics
+              </p>
+              {!user &&
+                <div id="login-form" className="login-form">
+                  <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebaseApp.auth()} />
+                </div>
+              }
+            </header>
+          </div>
+        </Route>
+        <Route path='/test'>
+          {user ?
+            <div className="App">
+              <header className="App-header">
+                {user &&
+                  <div>
+                    <button onClick={() => firebaseApp.auth().signOut()}>Sign Out</button>
+                    <p>Welcome {user.displayName}</p>
+                  </div>
+                }
+                <p>
+                  TESTING ROUTER - ONLY IF LOGGED IN
+              </p>
+                <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn React
+              </a>
+                {!user &&
+                  <div id="login-form" className="login-form">
+                    <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebaseApp.auth()} />
+                  </div>
+                }
+              </header>
+            </div> : <Redirect to='/' />}
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
