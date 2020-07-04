dc658589ae741d624ff3009df5bcee495cafc29c
Derryk Boyd <derrykboyd@gmail.com>
2020-07-04 00:59:25 -0600

    Convert dbUtils functions to async functions

--- src/Utils/dbUtils.js
+++ src/Utils/dbUtils.js
@@ -3,8 +3,9 @@ import { toast } from 'react-toastify';
 
 import { db } from '../App';
 
-export function addOpponent(userID, opponent) {
-  db.collection('users').doc(userID)
+// can use async when called
+export async function addOpponent(userID, opponent) {
+  return db.collection('users').doc(userID)
     .update({
       opponents: firestore.FieldValue.arrayUnion(opponent)
     })
@@ -15,61 +16,70 @@ export function addOpponent(userID, opponent) {
     .catch(e => console.error('Error adding opponent', e));
 }
 
-export function delGame(game) {
-  db.collection('users').doc(game.createdBy).collection('games')
-    .doc(game.gameID)
-    .delete()
-    .then(() => {
-      console.log('Game deleted');
-      toast.error('Game deleted form the database');
-    })
-    .catch(e => console.log('Error deleting game', e));
+// can also setup like this with async await
+export async function delGame(game) {
+  try {
+    await db.collection('users').doc(game.createdBy).collection('games')
+      .doc(game.gameID)
+      .delete();
+    console.log('Game deleted');
+    toast.error('Game deleted form the database');
+  }
+  catch (e) {
+    return console.log('Error deleting game', e);
+  }
 }
 
-export function delTeam(userID, teamID) {
-  db.collection('users').doc(userID)
-    .update({
-      [`teams.${teamID}`]: firebase.firestore.FieldValue.delete()
-    })
-    .then(() => {
-      console.log('Team deleted from database');
-      // Toast for successful delete
-      toast.error('Team Deleted');
-    })
-    .catch(e => console.error('Error deleting team', e))
+export async function delTeam(userID, teamID) {
+  try {
+    await db.collection('users').doc(userID)
+      .update({
+        [`teams.${teamID}`]: firebase.firestore.FieldValue.delete()
+      });
+    console.log('Team deleted from database');
+    // Toast for successful delete
+    toast.error('Team Deleted');
+  }
+  catch (e) {
+    return console.error('Error deleting team', e);
+  }
 }
 
-export function saveGame(game) {
-  db.collection('users').doc(game.createdBy).collection('games')
-    .doc(game.gameID)
-    .set(game)
-    .then(_ => {
-      console.log('Game saved');
-      toast.success('Game saved to the database');
-    })
-    .catch(e => console.error('Error saving game', e));
+export async function saveGame(game) {
+  try {
+    await db.collection('users').doc(game.createdBy).collection('games')
+      .doc(game.gameID)
+      .set(game);
+    console.log('Game saved');
+    toast.success('Game saved to the database');
+  }
+  catch (e) {
+    return console.error('Error saving game', e);
+  }
 }
 
-export function saveTeam(userID, newTeam, teamID) {
-  // update the User in the db from local state
-  db.collection('users').doc(userID)
-    .update({
-      [`teams.${teamID}`]: newTeam
-    })
-    .then(() => {
-      console.log('Team saved to database')
-      // Toast msg for successful save
-      toast.success('Team Saved');
-    })
-    .catch(e => console.error('Error updating teams', e))
+export async function saveTeam(userID, newTeam, teamID) {
+  try {
+    await db.collection('users').doc(userID)
+      .update({
+        [`teams.${teamID}`]: newTeam
+      });
+    console.log('Team saved to database');
+    toast.success('Team Saved');
+  }
+  catch (e) {
+    return console.error('Error updating teams', e);
+  }
 }
 
-export function updateUser(userID, newUser) {
-  db.collection('users').doc(userID)
-    .set(newUser)
-    .then(() => {
-      console.log('Database user doc updated');
-      toast.success('User saved to database');
-    })
-    .catch(e => console.error('Error updating user', e));
+export async function updateUser(userID, newUser) {
+  try {
+    await db.collection('users').doc(userID)
+      .set(newUser);
+    console.log('Database user doc updated');
+    toast.success('User saved to database');
+  }
+  catch (e) {
+    return console.error('Error updating user', e);
+  }
 }
